e8b61d0cff49c53083f1a555d7e238e129d7366c
SeungHyunH <vkflek1232@naver.com>
2021-10-14 13:38:30 +0000

    프로그래머스Lv3 - 카드 짝 맞추기(JS,BFS/DFS/백트랙킹)

--- programmers/Level3/카드짝맞추기/solution.js
+++ programmers/Level3/카드짝맞추기/solution.js
@@ -8,18 +8,14 @@ function solution(board, r, c) {
 
   const pairLen = pair.reduce((acc, cur) => acc + (cur.length === 0 ? 0 : 1), 0);
   const permutation = [];
-  const getPermutation = (arr, visit) => {
+  const getPermutation = (arr) => {
     if (arr.length === pairLen) { permutation.push(arr); return; }
     for (let i = 1; i <= 6; i++) {
       if (pair[i].length === 0) { continue; }
-      if (visit[i]) { continue; }
-      let temp = visit.slice();
-      temp[i] = true;
-      getPermutation([...arr, [...pair[i][0], ...pair[i][1]]], temp);
-      getPermutation([...arr, [...pair[i][1], ...pair[i][0]]], temp);
+      if (arr.includes(i)){continue;}
+      getPermutation([...arr,i]);
     }
   }
-  getPermutation([], Array(7).fill(false));
 
   const isMovable = (y, x) => { return 0 <= y && y <= 3 && 0 <= x && x <= 3 }
   const dy = [-1, 1, 0, 0];
@@ -69,29 +65,34 @@ function solution(board, r, c) {
     }
     return [sy, sx, Infinity];
   }
+
+  const backtrack = (sy,sx,k,count,board,permutation)=>{
+    if(k===pairLen){answer = (answer>count) ? count : answer;return;}
+    const idx = permutation[k];
+    const [ly,lx] = pair[idx][0];
+    const [ry,rx] = pair[idx][1];
+
+    let [ty1,tx1,cnt1] = BFS(sy,sx,ly,lx,board);
+    let [ty2,tx2,cnt2] = BFS(ty1,tx1,ry,rx,board);
+    board[ly][lx]=0;
+    board[ry][rx]=0;
+    backtrack(ty2,tx2,k+1,count+cnt1+cnt2,board,permutation);
+    board[ly][lx]=idx;
+    board[ry][rx]=idx;
+
+    [ty1,tx1,cnt1] = BFS(sy,sx,ry,rx,board);
+    [ty2,tx2,cnt2] = BFS(ty1,tx1,ly,lx,board);
+    board[ly][lx]=0;
+    board[ry][rx]=0;
+    backtrack(ty2,tx2,k+1,count+cnt1+cnt2,board,permutation);
+    board[ly][lx]=idx;
+    board[ry][rx]=idx;
+  }
+
   let answer = Infinity;
-  let tempBoard = Array.from(Array(4),()=>Array(4).fill(0));
-  permutation.forEach((per, idx) => {
-    pair.forEach((val,idx)=>{
-      if(val.length){
-        tempBoard[val[0][0]][val[0][1]] = idx+1;
-        tempBoard[val[1][0]][val[1][1]] = idx+1;
-      }
-    });
-    let cnt, count = 0;
-    let ty = r, tx = c;
-    per.forEach(cur => {
-      if(count>answer){return false;}
-      let [sy, sx, ey, ex] = cur;
-      [ty, tx, cnt] = BFS(ty, tx, sy, sx, tempBoard);
-      tempBoard[sy][sx] = 0;
-      count += cnt;
-      [ty, tx, cnt] = BFS(ty, tx, ey, ex, tempBoard);
-      count += cnt;
-      tempBoard[ey][ex] = 0;
-    });
-    answer = Math.min(answer, count);
-  });
+  getPermutation([]);
+  const new_board = board.slice();
+  permutation.forEach(per=>backtrack(r,c,0,0,new_board,per));
 
   return answer;
 }
