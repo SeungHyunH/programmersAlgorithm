34efd61dee7b18c871750befebfb6f574c938977
SeungHyunH <vkflek1232@naver.com>
2022-02-03 05:48:20 +0000

    프로그래머스Lv2 - 순위검색 (JS,이진탐색,조합)

--- programmers/Level2/순위검색/solution.js
+++ programmers/Level2/순위검색/solution.js
@@ -1,3 +1,4 @@
+/* 
 function solution(info, query) {
   //처음 제출했던 답(정확도 40/40 , 효율성 0/60)
   var answer = [];
@@ -19,56 +20,94 @@ function solution(info, query) {
   var answer = [];
   let infoMap = {};
 
-  function makeInfoMap(arr,score,start){
+  function makeInfoMap(arr, score, start) {
     const key = arr.join('');
     const value = infoMap[key];
 
-    if(value){
+    if (value) {
       infoMap[key].push(score);
-    }else{
-      infoMap[key]=[score];
+    } else {
+      infoMap[key] = [score];
     }
 
-    for(let i = start; i < arr.length; i++){
+    for (let i = start; i < arr.length; i++) {
       let temp = [...arr];
-      temp[i]='-';
-      makeInfoMap(temp,score,i+1);
+      temp[i] = '-';
+      makeInfoMap(temp, score, i + 1);
     }
   }
 
-  for(let i = 0; i < info.length; i++){
+  for (let i = 0; i < info.length; i++) {
     const arr = info[i].split(' ');
     const score = +arr.pop();
-    makeInfoMap(arr,score,0);
+    makeInfoMap(arr, score, 0);
   }
 
-  for(let key in infoMap){
-    infoMap[key].sort((a,b)=>a-b);
+  for (let key in infoMap) {
+    infoMap[key].sort((a, b) => a - b);
   }
 
-  function binarySearch(arr,score){
+  function binarySearch(arr, score) {
     let start = 0;
-    let end = arr.length-1;
-    while(start <= end){
-      const mid = Math.floor((end+start)/2);
-      if(arr[mid]<score){
-        start = mid+1;
-      }else{
-        end = mid-1;
+    let end = arr.length - 1;
+    while (start <= end) {
+      const mid = Math.floor((end + start) / 2);
+      if (arr[mid] < score) {
+        start = mid + 1;
+      } else {
+        end = mid - 1;
       }
     }
     return start;
   }
-  for(let i = 0; i < query.length; i++){
-    let arr = query[i].replace(/ and /g,' ').split(' ');
+  for (let i = 0; i < query.length; i++) {
+    let arr = query[i].replace(/ and /g, ' ').split(' ');
     const score = +arr.pop();
     arr = arr.join('');
-    if(infoMap[arr]){
-      answer.push(infoMap[arr].length - binarySearch(infoMap[arr],score));
-    }else{
+    if (infoMap[arr]) {
+      answer.push(infoMap[arr].length - binarySearch(infoMap[arr], score));
+    } else {
       answer.push(0);
     }
   }
+  return answer;
+}
+*/
+
+function solution(info, query) {
+  const data = {};
+  const answer = [];
+  for (let a of ['java', 'python', 'cpp', '-'])
+    for (let b of ['backend', 'frontend', '-'])
+      for (let c of ['junior', 'senior', '-'])
+        for (let d of ['chicken', 'pizza', '-'])
+          data[a + b + c + d] = [];
+  info.forEach(el => {
+    el = el.split(' ');
+    for (let a of [el[0], '-'])
+      for (let b of [el[1], '-'])
+        for (let c of [el[2], '-'])
+          for (let d of [el[3], '-'])
+            data[a + b + c + d].push(+el[4])
+  });
+  for (let key in data) { data[key].sort((a, b) => a - b); }
+  function BinarySearch(target, arr) {
+    let start = 0;
+    let end = arr.length;
+    let mid = 0;
+    while (start <= end) {
+      mid = Math.floor((start + end) / 2);
+      arr[mid] < target ? start = mid + 1 : end = mid - 1;
+    }
+    return start;
+  }
+  query.forEach(el => {
+    el = el.replace(/ and /g, ' ').split(' ');
+    let key = el[0] + el[1] + el[2] + el[3]
+    let find = +el[4]
+    let idx = BinarySearch(find, data[key])
+    answer.push(data[key].length - idx)
+  })
   return answer;
 }
 
